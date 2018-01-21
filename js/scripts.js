dd75b6c04becbcdd054558a66b92416eda920e10
Kasia Kowalska <k.kowalska68@yahoo.com>
2018-01-22 00:41:50 +0100

    Modify js scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,9 @@
-var list = document.getElementById('list');
-var add = document.getElementById('addElem');
+var list = document.getElementById('js-list');
+var add = document.getElementById('js-addElem');
 add.addEventListener('click', function() {
 	var element = document.createElement('li');
 	list.appendChild(element);
 	var newList = document.getElementsByTagName('li');
-	element.innerHTML = 'item' + (newList.length - 1);
+	element.innerHTML = 'item ' + (newList.length - 1);
 	console.log(list);
 } );
